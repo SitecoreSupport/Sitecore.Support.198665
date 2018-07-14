46b9c77d5d88d997bbb4bdc58d5ac2754ca66f6c
DK\psk <psk@sitecore.net>
2018-07-14 13:24:26 +0300

    Preparation

---
+++ src/Sitecore.Support.198665/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.198665")]
+[assembly: AssemblyProduct("Sitecore.Support.198665")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Scrollbars disappear after editing iext with RichTextEditor")]
+
